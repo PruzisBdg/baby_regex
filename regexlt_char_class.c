34eede40c6b32eed1f14f65e414f69eefb6f26b8
SPJE6500 <spjh1@wi.rr.com>
2018-02-28 22:30:38 -0600

    Adds negated char classes 'DWS'

--- regexlt_char_class.c
+++ regexlt_char_class.c
@@ -29,9 +29,13 @@ typedef struct {
 
 // These char class definitions are legal OUTSIDE a '[... ]'.
 PRIVATE S_ClassMap const prebakedCharClasses[] = {
-   { 'd', "[0-9]"          },
-   { 'w', "[0-9A-Za-z_]"   },
-   { 's', "[ \t\r\n]"      }
+   { 'd', "[0-9]"           },
+   { 'w', "[0-9A-Za-z_]"    },
+   { 's', "[ \t\r\n]"       },
+   // Negated classes.
+   { 'D', "[^0-9]"          },
+   { 'W', "[^0-9^A-Z^a-z_]" },
+   { 'S', "[^ ^\t^\r^\n]"   }
 };
 
 PUBLIC C8 const *regexlt_getCharClassByKey(C8 key)
@@ -58,9 +62,18 @@ PUBLIC void regexlt_classParser_Init(S_ParseCharClass *p)
    p->negate = FALSE;   // Until there's a '^'
    p->range = FALSE;    // Until there's e.g A-Z
    p->esc = FALSE;      // Until there's a '\'.
+   p->hex.step = 0;     // Until we hit 'x' in e.g '\x25'
 }
 
+/* ---------------------------------- addOrRemove -------------------------------------- */
 
+PUBLIC void addOrRemove(S_ParseCharClass *p, S_C8bag *cc, C8 newCh)
+{
+   if(p->negate == TRUE)                     // To be removed from existing range?
+      { C8bag_RemoveOne(cc, newCh); p->negate = FALSE; }   // We are done with this negation.
+   else                                      // else to be added to existing range
+      { C8bag_AddOne(cc, newCh); }
+}
 
 /* ------------------------------------ regexlt_classParser_AddCh ------------------------------------
 
@@ -71,129 +84,124 @@ PUBLIC void regexlt_classParser_Init(S_ParseCharClass *p)
    illegal, or E_Continue if 'newCh' has been added and we need more input to complete the
    class.
 */
-
 PUBLIC T_ParseRtn regexlt_classParser_AddCh(S_ParseCharClass *p, S_C8bag *cc, C8 newCh)
 {
    switch(newCh)
    {
-      case '\0':                                      // Incomplete class expression -> always fail
-      case '[':                                       // Cannot nest classes -> always fail
+      case '\0':                                      // ... Incomplete class expression -> always fail
+      case '[':                                       // ... Cannot nest classes -> always fail
          return E_Fail;
 
       case ']':                                       // Closing a class?...
-         if(p->range == TRUE || p->negate == TRUE)    // ...but did not complete range or negation?
-         {
-            return E_Fail;                            // then fail
-         }
-         else                                         // else legal completion of a class
-         {
-            return E_Complete;
-         }
+         if(p->range == TRUE || p->negate == TRUE)             // ...but did not complete range or negation?
+            { return E_Fail; }                                 // then fail
+         else                                                  // else legal completion of a class
+            { return E_Complete; }
          break;
 
       case '-':                                       // ...Range char?
-         if(p->esc == TRUE)                           // Prev was esc?
+         if(p->esc == TRUE)                                    // Prev was esc?
          {
-            C8bag_AddOne(cc, '-');                    // then it's a literal '-'. Add it to current class
-            p->esc = FALSE;                           // and we have used the ESC.
+            C8bag_AddOne(cc, '-');                             // then it's a literal '-'. Add it to current class
+            p->esc = FALSE;                                    // and we have used the ESC.
          }
          else if(p->prevCh == '[' || p->prevCh == '-' || p->prevCh == '^')   // But previous char was not a letter/number whatever?
-         {
-            return E_Fail;                            // so range is missing it's start -> fail
-         }
+            { return E_Fail; }                                 // so range is missing it's start -> fail
          else
-         {
-            p->range = TRUE;                          // else we are specifying a range now; continue.
-         }
+            { p->range = TRUE; }                               // else we are specifying a range now; continue.
          break;
 
-      case '^':                                       // ...Negation?
-         if(p->esc == TRUE)                           // Prev was esc?
+      case '^':                                       // ... Negation?
+         if(p->esc == TRUE)                                    // Prev was esc?
          {
-            C8bag_AddOne(cc, '^');                    // then it's a literal '^'. Add it to current class
-            p->esc = FALSE;                           // and we have used the ESC.
+            C8bag_AddOne(cc, '^');                             // then it's a literal '^'. Add it to current class
+            p->esc = FALSE;                                    // and we have used the ESC.
          }
-         else if(p->prevCh == '-' || p->prevCh == '^' || // But just opened a negation or range? OR
-            p->negate == TRUE)                        // were already negating (a range)?
-         {
-            return E_Fail;                            // then illegal construction -> fail
-         }
+         else if(p->prevCh == '-' || p->prevCh == '^' ||       // But just opened a negation or range? OR
+            p->negate == TRUE)                                 // were already negating (a range)?
+            { return E_Fail; }                                 // then illegal construction -> fail
          else
          {
-            p->negate = TRUE;                          // else we are negating now; continue.
-         }
-         break;
+            p->negate = TRUE;                                  // else we are negating now; continue.
 
-      case '\\':                                      // Opens a pre-defined character class or an in-range escaped literal.
-         if(p->prevCh == '-' || p->range == TRUE)     // But we are specifying a range?... can't put a pre-defined class within that..
-         {
-            return E_Fail;                            // so fail
-         }
-         else
-         {
-            p->esc = TRUE;                            // else mark the escape, to be followed, presumably, by the class specifier.
-         }
-         break;
-#if 0
-      case '.':                                       // Includes everything
-         if(p->prevCh == '-' || p->range == TRUE)     // Again, not legal in a range-specifier
-         {
-            return E_Fail;                            // so fail
-         }
-         else                                         // otherwise good... make a class with all printables & whitespace.
-         {
-            S_ParseCharClass pc;
-            classParser_Init(&pc);
-            if( classParser_AddDef(&pc, cc, allPrintablesAndWhtSpc) == TRUE ) {  // Made the new class?
-               break;                                 // ... success, continue
+            if(p->prevCh == '[')                               // Is '[^...?
+            {                                                  // means we start with everything and will subtract members
+               C8bag_Invert(cc);                               // so invert the initial empty class to make a full one.
             }
-            else                                      // else failed to make new class
+         }
+         break;
+
+      case '\\':                                      // ... Opens a pre-defined character class or an in-range escaped literal.
+         if(p->prevCh == '-' || p->range == TRUE)              // But we are specifying a range?... can't put a pre-defined class within that..
+            { return E_Fail; }                                 // so fail
+         else
+            { p->esc = TRUE; }                                 // else mark the escape, to be followed, presumably, by the class specifier.
+         break;
+
+      default:                                        // ... some other letter/number whatever.
+         if(p->hex.step > 0)                                   // Parsing a hex e.g '\x4c'?
+         {
+            if (!IsHexASCII(newCh) )                           // But this next char isn't Hex ASCII?
+               { return E_Fail; }                              // So fail rightaway
+            else
             {
-               return E_Fail;                         // ejected!!
-            }
-         }
-         break;
-#endif
-      default:                                        // ...just a letter/number whatever.
-         if(p->esc == TRUE)                           // Which was escaped?
-         {
-            p->esc = FALSE;                           // Will handle ESC; clear flag
-            C8 const *def;
-
-            if((def = getCharClassByKey(newCh)) != NULL)    // Is a legal escape char?
-            {                                               // then will make a new class corresponding to the char
-               S_ParseCharClass pc;                         // We are already inside a class parser; make a fresh private one.
-               classParser_Init(&pc);
-               if( classParser_AddDef(&pc, cc, def) == TRUE ) {   // Made the new class?
-                  break;                                    // success, continue
+               if(p->hex.step == 1)                            // HexASCII for high nibble
+               {
+                  p->hex.hi = HexToNibble(newCh);              // read it.
+                  p->hex.step = 2;
+               }
+               else                                            // else HexASCII for low nibble.
+               {                                               // Convert to byte and add/remove from bag.
+                  C8 hexascii = (p->hex.hi << 4) + HexToNibble(newCh);
+                  addOrRemove(p, cc, hexascii);
+                  p->prevCh = hexascii;                        // Record this hexascii, even though we added it. It may be the start of a range e.g '\x33-\x44'.
+                  p->hex.step = 0;                             // and we are done reading hex.
                }
             }
-            return FALSE;
          }
-         else if(p->range == TRUE)                    // We were specifying a range?
+         else if(p->esc == TRUE)                               // Some escaped char?
          {
-            p->range = FALSE;                         // then we close it now.
+            p->esc = FALSE;                                    // Will handle ESC; clear flag
 
-            if(newCh < p->prevCh)                     // End-of-range is less than start?
+            if(newCh == 'x')                                   // '\x'?
             {
-               return E_Fail;                         // that's a fail right there.
+               p->hex.step = 1;                                // this starts a hex number e.g '\x9a'
+               return E_Continue;
             }
-            else                                      // else legal range.
+            else                                               // else it should be some 'escaped' class e.g '\D'
             {
-               if(p->negate == TRUE)                  // Remove these chars from the range specified so far?
-                  { C8bag_RemoveRange(cc, p->prevCh, newCh); p->negate = FALSE; }    // This negation is done.
-               else                                   // else add to any specified so far.
-                  { C8bag_AddRange(cc, p->prevCh, newCh); }
+               C8 const *def;
+
+               if((def = getCharClassByKey(newCh)) != NULL)    // Is a legal class-specifier
+               {                                               // then will make a new class corresponding to the char
+                  S_ParseCharClass pc;                         // We are already inside a class parser; make a fresh private one.
+                  classParser_Init(&pc);
+                  if( classParser_AddDef(&pc, cc, def) == TRUE ) {   // Made the new class?
+                     break;                                    // success, continue
+                  }
+               }
+               return E_Fail;
             }
          }
-         else                                         // else not a range; just a lone char.
+         else if(p->prevCh == '[' && newCh == '.')             // Leading '.'?
+         {                                                     // means we start with everything and will subtract members
+            C8bag_Invert(cc);                                  // so invert the initial empty class to make a full one.
+         }
+         else if(p->range == TRUE)                             // We were specifying a range?
          {
-            if(p->negate == TRUE)                     // To be removed from existing range?
-               { C8bag_RemoveOne(cc, newCh); p->negate = FALSE; }   // We are done with this negation.
-            else                                      // else to be added to existing range
-               { C8bag_AddOne(cc, newCh); }
+            p->range = FALSE;                                  // then we close it now.
+
+            if(newCh < p->prevCh)                              // End-of-range is less than start?
+               { return E_Fail; }                              // that's a fail right there.
+            else {                                             // else legal range.
+               if(p->negate == TRUE)                           // Remove these chars from the range specified so far?
+                  { C8bag_RemoveRange(cc, p->prevCh, newCh); p->negate = FALSE; }    // This negation is done.
+               else                                            // else add to any specified so far.
+                  { C8bag_AddRange(cc, p->prevCh, newCh); }}
          }
-         p->prevCh = newCh;            // Remember this char for next time.
+         else                                                  // else not a range; just a lone char.
+            { addOrRemove(p, cc, newCh); }
+         p->prevCh = newCh;                                    // Remember this char for next time.
    }
    return E_Continue;   // Didn't fail or complete above, so continue.
 }
